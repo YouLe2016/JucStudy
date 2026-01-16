8d5744371612e8736480542c58c0da6c22701f66
uupt <270628297@qq.com>
2026-01-16 21:08:44 +0800

    Add: VendingMachineV2。单元测试

--- src/test/kotlin/designpatterns/state/v2/VendingMachineV2Test.kt
+++ src/test/kotlin/designpatterns/state/v2/VendingMachineV2Test.kt
@@ -55,10 +55,60 @@ class VendingMachineV2Test {
         )
     }
 
-//    @Test
-//    fun testSelectProduct() {
-//        vendingMachine.selectProduct()
-//    }
+    @Test
+    fun testSelectProduct_idle() {
+        vendingMachine.selectProduct("1024")
+        assertEquals(
+            expected = IdleState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+        vendingMachine.selectProduct("1025")
+        assertEquals(
+            expected = IdleState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+    }
+    @Test
+    fun testSelectProduct_hasMoney() {
+        // 进入HAS_MONEY 状态
+        vendingMachine.insertMoney()
+        vendingMachine.selectProduct("1024")
+        assertEquals(
+            expected = IdleState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+
+        // 进入HAS_MONEY 状态
+        vendingMachine.insertMoney()
+        vendingMachine.selectProduct("1025")
+        assertEquals(
+            expected = HasMoneyState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+    }
+
+    @Test
+    fun testSelectProduct_outOfStock() {
+        // 强行设置状态为 OUT_OF_STOCK (因为没有正常逻辑能到达这个状态)
+        setPrivateProperty(vendingMachine, "state", OutOfStockState)
+        vendingMachine.selectProduct("1024")
+        assertEquals(
+            expected = OutOfStockState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 OUT_OF_STOCK"
+        )
+        vendingMachine.selectProduct("1025")
+        assertEquals(
+            expected = OutOfStockState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 OUT_OF_STOCK"
+        )
+    }
+
 //
 //    @Test
 //    fun testRequestRefund() {
