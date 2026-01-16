dd16f2a4c827286606836742e6be239f81cf9ff3
uupt <270628297@qq.com>
2026-01-16 21:24:34 +0800

    Add: VendingMachineV3。单元测试

--- src/test/kotlin/designpatterns/state/v3/VendingMachineV3Test.kt
+++ src/test/kotlin/designpatterns/state/v3/VendingMachineV3Test.kt
@@ -23,7 +23,7 @@ class VendingMachineV3Test {
 
     @Test
     fun testInsertMoney_idle() {
-        vendingMachine.process(VendingIntent.InsertMoney)
+        vendingMachine.insertMoney()
         assertEquals(
             expected = HasMoneyState,
             actual = getPrivateProperty(vendingMachine, "state"),
@@ -34,8 +34,8 @@ class VendingMachineV3Test {
     @Test
     fun testInsertMoney_hasMoney() {
         // 进入HAS_MONEY 状态
-        vendingMachine.process(VendingIntent.InsertMoney)
-        vendingMachine.process(VendingIntent.InsertMoney)
+        vendingMachine.insertMoney()
+        vendingMachine.insertMoney()
         assertEquals(
             expected = HasMoneyState,
             actual = getPrivateProperty(vendingMachine, "state"),
@@ -47,7 +47,7 @@ class VendingMachineV3Test {
     fun testInsertMoney_OutOfStock() {
         // 强行设置状态为 OUT_OF_STOCK (因为没有正常逻辑能到达这个状态)
         setPrivateProperty(vendingMachine, "state", OutOfStockState)
-        vendingMachine.process(VendingIntent.InsertMoney)
+        vendingMachine.insertMoney()
         assertEquals(
             expected = OutOfStockState,
             actual = getPrivateProperty(vendingMachine, "state"),
@@ -55,11 +55,60 @@ class VendingMachineV3Test {
         )
     }
 
-//    @Test
-//    fun testSelectProduct() {
-//        vendingMachine.process(VendingIntent.SelectProduct)
-//    }
-//
+    @Test
+    fun testSelectProduct_idle() {
+        vendingMachine.selectProduct("1024")
+        assertEquals(
+            expected = IdleState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+        vendingMachine.selectProduct("1025")
+        assertEquals(
+            expected = IdleState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+    }
+    @Test
+    fun testSelectProduct_hasMoney() {
+        // 进入HAS_MONEY 状态
+        vendingMachine.insertMoney()
+        vendingMachine.selectProduct("1024")
+        assertEquals(
+            expected = IdleState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+
+        // 进入HAS_MONEY 状态
+        vendingMachine.insertMoney()
+        vendingMachine.selectProduct("1025")
+        assertEquals(
+            expected = HasMoneyState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 IDLE"
+        )
+    }
+
+    @Test
+    fun testSelectProduct_outOfStock() {
+        // 强行设置状态为 OUT_OF_STOCK (因为没有正常逻辑能到达这个状态)
+        setPrivateProperty(vendingMachine, "state", OutOfStockState)
+        vendingMachine.selectProduct("1024")
+        assertEquals(
+            expected = OutOfStockState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 OUT_OF_STOCK"
+        )
+        vendingMachine.selectProduct("1025")
+        assertEquals(
+            expected = OutOfStockState,
+            actual = getPrivateProperty(vendingMachine,"state"),
+            message = "状态应保持不变 OUT_OF_STOCK"
+        )
+    }
+
 //    @Test
 //    fun testRequestRefund() {
 //        vendingMachine.process(VendingIntent.RequestRefund)
